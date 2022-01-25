bd2f3d84ae8b131d618d3ed10fec8deb260035cc
Gaurav Gupta <70667541+gauravjicoder@users.noreply.github.com>
2022-01-25 15:48:00 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Footer.css"
+const Footer = () => {
+  return (
+  <footer>
+          <p>Create by Gaurav Contact-<span>7678451481</span></p>
+  </footer>
+  )
+};
+
+export default Footer;
